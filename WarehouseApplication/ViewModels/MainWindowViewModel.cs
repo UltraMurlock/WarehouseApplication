eede6249a1c23d551f7d8e1dfc73506f55a8a520
UltraMurlock <egorplettnev00@gmail.com>
2024-10-03 15:18:46 +0300

    Добавлена валидация вводимых идентификаторов

--- WarehouseApplication/ViewModels/MainWindowViewModel.cs
+++ WarehouseApplication/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 using System.Windows;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -15,6 +16,8 @@ namespace WarehouseApplication.ViewModels
 
         private InputOutputModel _model;
 
+        private readonly Regex _idRegex;
+
 
 
         public MainWindowViewModel()
@@ -22,30 +25,56 @@ namespace WarehouseApplication.ViewModels
             _model = new InputOutputModel();
             AddInputCommand = new DelegateCommand<string>((string id) => AddInput(id));
             AddOutputCommand = new DelegateCommand<string>((string id) => AddOutput(id));
+
+            _idRegex = new Regex(@"^([0-9]|[A-F])*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         }
 
 
 
         private void AddInput(string id)
         {
-            if(string.IsNullOrEmpty(id))
-            {
-                MessageBox.Show("Ошибка в поле ID приёма");
+            if(!TryParseIdText(id, out var ids))
                 return;
-            }
 
-            _model.AddInputProducts(id);
+            _model.AddInputProducts(ids);
         }
 
         private void AddOutput(string id)
         {
-            if(string.IsNullOrEmpty(id))
-            {
-                MessageBox.Show("Ошибка в поле ID отгрузки");
+            if(!TryParseIdText(id, out var ids))
                 return;
+
+            _model.AddOutputProducts(ids);
+        }
+
+
+
+        private bool TryParseIdText(string idText, out string[] ids)
+        {
+            ids = new string[0];
+            if(string.IsNullOrEmpty(idText))
+            {
+                MessageBox.Show("Введите в поле ввода идентификатор");
+                return false;
             }
 
-            _model.AddOutputProducts(id);
+            ids = idText.Split(' ');
+            foreach(string id in ids)
+            {
+                if(id.Length != 24)
+                {
+                    MessageBox.Show($"Ошибка в идентификаторе {id}. Длина идентификатора должна быть равна 24 символам");
+                    return false;
+                }
+
+                if(!_idRegex.IsMatch(id))
+                {
+                    MessageBox.Show($"Ошибка в идентификаторе {id}. Должны использоваться только HEX-символы (0-9 и A-F)");
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
